8efb1c695f3a8da6052436f90339e712e264b0df
arisolgo <arielsolano39@gmail.com>
2020-04-01 23:54:29 -0400

    backend instance added and address service created

---
+++ frontend/ormFrontend/src/app/services/address.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { throwError } from 'rxjs/internal/observable/throwError';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddressService {
+  baseUrl = 'http://localhost:4000';
+  allAddress: any = [];
+  iphoneFlgFrc = true;
+
+  constructor(private http: HttpClient) {}
+
+  getAddress() {
+    this.allAddress = this.http.get(this.baseUrl + '/address');
+    return this.allAddress;
+  }
+
+  getAddressById(id) {
+    return this.http
+      .get(this.baseUrl + '/address/' + id);
+  }
+
+  addAddress(formData) {
+    const postData = JSON.stringify(formData);
+    return this.http.post(this.baseUrl + '/address/', postData);
+  }
+
+  updateAddress(id, formData) {
+    const postData = JSON.stringify(formData);
+   
+    return this.http.put(
+      this.baseUrl +  '/address/' + id, postData);
+  }
+
+  deleteAddress(id) {
+    return this.http.delete(this.baseUrl + '/address/' + id);
+  }
+
+ 
+}
