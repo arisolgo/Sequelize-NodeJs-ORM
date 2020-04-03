575bffa6ad1446ac91600b4a3bc83585be415d6b
arisolgo <arielsolano39@gmail.com>
2020-04-02 23:57:52 -0400

    project page almost ready

--- frontend/ormFrontend/src/app/projects/projects.page.ts
+++ frontend/ormFrontend/src/app/projects/projects.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectService } from '../services/project.service';
+import { Router } from '@angular/router';
+import { EmployeeService } from '../services/employee.service';
 
 @Component({
   selector: 'app-projects',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsPage implements OnInit {
 
-  constructor() { }
+  projects:any = [];
+  constructor(private projectService:ProjectService, private router:Router, private employeeService:EmployeeService) { }
 
   ngOnInit() {
+    this.getAllProjects();
   }
+
+  getAllProjects(){
+      this.projectService.getAllProjects().subscribe(result=>{
+          this.projects = result;
+          
+          this.projects.forEach(element => {
+              this.employeeService.getEmployee(element.leaderId).subscribe(result=>{
+                  element['leader'] = result;
+              })
+          });
+      })
+  }
+
+  goToProjectDetail(project){
+    this.router.navigate(['/project-detail'+ project.id]);
+  }
+  
+  
 
 }
